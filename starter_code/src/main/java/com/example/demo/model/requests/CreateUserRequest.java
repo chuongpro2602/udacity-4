8891147a8dac89ca1414324402acab922541fd4e
chuongpro2602 <dinhngocchuong123@gmail.com>
2023-10-04 16:36:54 +0700

    fix lombok
    fix lombok

--- starter_code/src/main/java/com/example/demo/model/requests/CreateUserRequest.java
+++ starter_code/src/main/java/com/example/demo/model/requests/CreateUserRequest.java
@@ -2,15 +2,6 @@ package com.example.demo.model.requests;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class CreateUserRequest {
 
 	@JsonProperty
@@ -21,5 +12,29 @@ public class CreateUserRequest {
 
 	@JsonProperty
 	private String confirmPassword;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
 
 }
